bfd30eeff9da644bf6a496925c4eae12c475ae22
Karen Daniela <kdaniimora@gmail.com>
2020-03-04 13:29:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,8 +8,11 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
  
-courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
- courses.each do |course|
-   puts 
-   course.text.strip
- end 
+#courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+ #courses.each do |course|
+  # puts 
+  # course.text.strip
+# end 
+
+
+p doc.css
